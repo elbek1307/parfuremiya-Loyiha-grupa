eb6665c4b7cad14b4fa645fd6d6b44b79cf7faed
Mahliyo <gasandigital13@gmail.com>
2026-06-16 09:06:57 +0500

    sectionOne star wrapper css responsive

--- src/Components/SectionTwo/SectionTwo.jsx
+++ src/Components/SectionTwo/SectionTwo.jsx
@@ -1,6 +1,11 @@
 import { useTranslation } from "react-i18next";
 import "./SectionTwo.css";
 import React from 'react'
+import two1 from "./image/two1.png";
+import two1 from "./image/two1.png";
+import two1 from "./image/two1.png";
+import two1 from "./image/two1.png";
+import two1 from "./image/two1.png";
 
 function SectionTwo() {
     const {t} = useTranslation();
